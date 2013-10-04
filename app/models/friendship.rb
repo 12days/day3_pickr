a81af80f6f75aec2a23303d90591c8c4242b655a
Joshua Wilborn <Joshua.J.Wilborn@gmail.com>
2013-10-03 20:22:54 -0700

    user#add_friend added, friendship validation added

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,11 @@
 class Friendship < ActiveRecord::Base
+  validates_uniqueness_of :user_id, scope: :friend_id
+  validate :friend_is_not_self
+
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  def friend_is_not_self
+    errors.add("Cannot friend yourself.") if friend_id == user_id
+  end
 end
